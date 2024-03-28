e17263515fb6d01df8afd4364c79d33d5425faee
SiongYu <loosiongyu@hotmail.com>
2024-03-28 22:00:24 +0800

    Updated IRSend Code

--- IRremote/IRsendDemo/IRsendDemo.ino
+++ IRremote/IRsendDemo/IRsendDemo.ino
@@ -31,27 +31,47 @@
 #include <Arduino.h>
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
+#include <M5StickCPlus.h>
 
 const uint16_t kIrLed = 33;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
 
 IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
 
+
 // Example of data captured by IRrecvDumpV2.ino
-uint16_t rawData[67] = {9000, 4500, 650, 550, 650, 1650, 600, 550, 650, 550,
-                        600, 1650, 650, 550, 600, 1650, 650, 1650, 650, 1650,
-                        600, 550, 650, 1650, 650, 1650, 650, 550, 600, 1650,
-                        650, 1650, 650, 550, 650, 550, 650, 1650, 650, 550,
-                        650, 550, 650, 550, 600, 550, 650, 550, 650, 550,
-                        650, 1650, 600, 550, 650, 1650, 650, 1650, 650, 1650,
-                        650, 1650, 650, 1650, 650, 1650, 600};
+// uint16_t rawData[67] = {9000, 4500, 650, 550, 650, 1650, 600, 550, 650, 550,
+//                         600, 1650, 650, 550, 600, 1650, 650, 1650, 650, 1650,
+//                         600, 550, 650, 1650, 650, 1650, 650, 550, 600, 1650,
+//                         650, 1650, 650, 550, 650, 550, 650, 1650, 650, 550,
+//                         650, 550, 650, 550, 600, 550, 650, 550, 650, 550,
+//                         650, 1650, 600, 550, 650, 1650, 650, 1650, 650, 1650,
+//                         650, 1650, 650, 1650, 650, 1650, 600};
+
+uint16_t rawData[67] = {4526, 4476,  576, 1660,  574, 1662,  568, 1670,  
+                        574, 544,  576, 542,  568, 548,  572, 548,  576, 
+                        542,  578, 1660,  572, 1666,  568, 1668,  574, 
+                        542,  576, 540,  570, 548,  572, 546,  574, 546,  
+                        576, 542,  570, 1666,  574, 544,  578, 540,  570, 
+                        550,  570, 546,  574, 544,  576, 542,  570, 1666,  
+                        576, 542,  568, 1670,  572, 1664,  568, 1668,  576, 1662,  
+                        568, 1668,  574, 1660,  572};  // SAMSUNG TV
 
 // Example Samsung A/C state captured from IRrecvDumpV2.ino
-uint8_t samsungState[kSamsungAcStateLength] = {
-    0x02, 0x92, 0x0F, 0x00, 0x00, 0x00, 0xF0,
-    0x01, 0xE2, 0xFE, 0x71, 0x40, 0x11, 0xF0};
+// uint8_t samsungState[kSamsungAcStateLength] = {
+//     0x02, 0x92, 0x0F, 0x00, 0x00, 0x00, 0xF0,
+//     0x01, 0xE2, 0xFE, 0x71, 0x40, 0x11, 0xF0};
 
 void setup() {
   irsend.begin();
+  M5.begin();
+
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 0, 2);
+  M5.Lcd.printf("Sending Start", 0);
+
+  pinMode(M5_BUTTON_HOME, INPUT);
+  pinMode(M5_LED, OUTPUT); // setting the M5_LED as output mode
 #if ESP8266
   Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);
 #else  // ESP8266
@@ -66,9 +86,12 @@ void loop() {
   // Serial.println("Sony");
   // irsend.sendSony(0xa90, 12, 2);  // 12 bits & 2 repeats
   // delay(2000);
-  Serial.println("a rawData capture from IRrecvDumpV2");
-  irsend.sendRaw(rawData, 73, 38);  // Send a raw data capture at 38kHz.
-  delay(2000);
+  if(digitalRead(M5_BUTTON_HOME) == LOW){
+    M5.Lcd.print("Sending Signal");
+    irsend.sendRaw(rawData, 67, 38);
+    digitalWrite(M5_LED, !digitalRead(M5_LED));
+    while(digitalRead(M5_BUTTON_HOME) == LOW);
+  }
   // Serial.println("a Samsung A/C state from IRrecvDumpV2");
   // irsend.sendSamsungAC(samsungState);
   // delay(2000);
