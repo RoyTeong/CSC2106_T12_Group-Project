18e137d6417522aa2c7c06c2097eb4fff08e87b7
SiongYu <loosiongyu@hotmail.com>
2024-04-01 19:49:10 +0800

    removed error

--- IRremote/sketch_IRRecv/sketch_IRRecv.ino
+++ IRremote/sketch_IRRecv/sketch_IRRecv.ino
@@ -199,19 +199,6 @@ void loop() {
     // Display the basic output of what we found.
     Serial.print(resultToHumanReadableBasic(&results));
 
-    // Compare the received raw data with each expected raw data
-    bool isTVMatch = std::equal(std::begin(results.rawbuf), std::end(results.rawbuf), std::begin(TV_rawData));
-    bool isAC_ON_Match = std::equal(std::begin(results.rawbuf), std::end(results.rawbuf), std::begin(AC_ON_rawData));
-    bool isAC_OFF_Match = std::equal(std::begin(results.rawbuf), std::end(results.rawbuf), std::begin(AC_OFF_rawData));
-    
-    if (isTVMatch) {
-        M5.Lcd.printf("Received TV signal", 0);
-    } else if (isAC_ON_Match) {
-        M5.Lcd.printf("Received AIRCON ON signal", 0);
-    } else if (isAC_OFF_Match) {
-        M5.Lcd.printf("Received AIRCON OFF signal", 0);
-    } 
-
     // Display any extra A/C info if we have it.
     String description = IRAcUtils::resultAcToString(&results);
     if (description.length()) Serial.println(D_STR_MESGDESC ": " + description);
